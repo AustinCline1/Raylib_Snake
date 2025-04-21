bb730079dc11c3d2e7b35594409d3ce329c4859e
Austin Cline <au.cline@hotmail.com>
2025-04-21 11:37:59 -0400

    Added food object that will spawn in a random position

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,50 @@
 Color green = {173,204,96,255};
 Color darkGreen = {43,51,24,255};
 
+
+int cellSize = 30; 
+int cellCount = 25;
+
+class Food 
+{
+
+public:
+	Vector2 position;
+	Texture2D texture;
+
+	Food()
+	{
+		Image image = LoadImage("graphics/food.png");
+		texture = LoadTextureFromImage(image);
+		UnloadImage(image);
+		position = GenerateRandomPosition();
+	}
+
+	~Food()
+	{
+		UnloadTexture(texture);
+	}
+
+	void Draw() 
+	{
+		DrawTexture(texture,position.x*cellSize,position.y*cellSize,WHITE);
+	}
+	
+	Vector2 GenerateRandomPosition()
+	{
+		float x = GetRandomValue(0,cellCount -1);
+		float y = GetRandomValue(0,cellCount -1);
+		return Vector2{x,y};
+	}
+
+};
+
+
 int main() 
 {
 	SetTargetFPS(60);
-
-	InitWindow(750,750, "Retro Snake");
+	InitWindow(cellSize*cellCount,cellSize*cellCount, "Retro Snake");
+	Food food = Food();
 		
 	while(WindowShouldClose() == false)
 	{
@@ -17,7 +56,7 @@ int main()
 
 	//Drawing
 	ClearBackground(green);
-	
+	food.Draw();
 	
 	EndDrawing();
 	}
